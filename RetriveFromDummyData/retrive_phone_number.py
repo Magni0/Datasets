f66a7b854af67b22f3f704cc9c62e7af6fefd117
Logan Harris <loganrodharris@gmail.com>
2021-06-02 16:46:46 +1000

    updated retrive_phone_number.py so that " is removed

--- RetriveFromDummyData/retrive_phone_number.py
+++ RetriveFromDummyData/retrive_phone_number.py
@@ -2,12 +2,14 @@ import json
 
 phone_number = []
 
-with open('Resorts.json', 'r') as file:
+with open('DummyData/Resorts.json', 'r') as file:
     raw = file.read()
     content = json.loads(raw)
     [phone_number.append(i['Phone']) for i in content if i['Phone'] not in phone_number]
 
+u = [i.replace('"', '') for i in phone_number]
+
 # write countrys to file
 with open('output/PhoneNumber.txt', 'a') as f:
-    for x in phone_number:
+    for x in u:
         f.write(x + "\n")
